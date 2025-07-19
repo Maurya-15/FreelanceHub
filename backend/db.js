1c86cef85da9900ab4aa56e90ccc77c38e19d51b
Maurya-15 <mauryadoshi1@gmail.com>
2025-07-19 22:36:41 +0530

    Commit Example

--- backend/db.js
+++ backend/db.js
@@ -98,4 +98,4 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+export default connectDB;backend/server.js
--- backend/server.js
+++ backend/server.js
@@ -211,4 +211,4 @@ io.on('connection', (socket) => {
 
 
   
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
